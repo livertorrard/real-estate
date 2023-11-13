fab5fdb3b199dd41bfeeed1fd0eba2181d255e4d
Thai Van Loi <loi.tv@hodfords.com>
2023-11-14 00:33:01 +0700

    test

--- real-estate-server/src/users/http/controller/user.controller.ts
+++ real-estate-server/src/users/http/controller/user.controller.ts
@@ -20,46 +20,52 @@ import { RegisterUserDto } from '../dto/register-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Controller('users')
-@UseGuards(AuthGuard)
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   searchUsers(@Query('search') keySearch: string) {
     return this.userService.searchUsers(keySearch);
   }
 
-  @Post('register')
+  @Post('registers')
   register(@Body() dto: RegisterUserDto) {
     return this.userService.register(dto);
   }
 
   @Post('active')
+  @UseGuards(AuthGuard)
   activeUser(@Body() dto: ActiveUserDto) {
     return this.userService.activeUser(dto);
   }
 
   @Post('create')
+  @UseGuards(AuthGuard)
   createUser(@Body() dto: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(dto);
   }
 
   @Delete('delete')
-  deleleUsers(@Body() dto: DeleteUserDto) {
+  @UseGuards(AuthGuard)
+  deleteUsers(@Body() dto: DeleteUserDto) {
     return this.userService.deleteUsers(dto.ids);
   }
 
   @Post(':id/change-password')
+  @UseGuards(AuthGuard)
   changePassword(@Body() dto: ChangePasswordDto, @Param('id') id: string) {
     return this.userService.changePassword(dto, id);
   }
 
   @Put(':id/edit')
+  @UseGuards(AuthGuard)
   updateUser(@Body() dto: UpdateUserDto) {
     return this.userService.updateUser(dto);
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   getUserById(@Param('id') id: string) {
     return this.userService.getUserById(id);
   }
